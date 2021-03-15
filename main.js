57f4742a8c90a883306db880cb96abe2d3c674b7
Andy Choe <Andrew.s.choe@gmail.com>
2021-03-15 15:59:19 -0400

    completed assign

---
+++ main.js
@@ -0,0 +1,14 @@
+document.querySelector('p').style.color = 'lightblue'
+
+document.querySelector('h1').style.fontSize = '48px'
+
+document.querySelector('p').innerHTML = '<p> Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.</p>'
+
+document.getElementById("item-13").style.opacity = '0.5'
+document.getElementById("item-3").innerHTML = '<li>I say, "Hi!"</li>'
+
+const image = document.querySelector('img')
+image.src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg'
+image.style.height = '300px'
+
+document.getElementById('item-16').innerHTML = 'Won\'t get fooled again.'
